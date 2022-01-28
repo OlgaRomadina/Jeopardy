a89d8f42a54d330d6cc1932c1629948677fa9381
Khasan <khas.yang.kelo@gmail.com>
2022-01-28 15:16:58 +0300

    изменен Card

--- jeopardy/src/Components/Question.jsx
+++ jeopardy/src/Components/Question.jsx
@@ -1,39 +1,39 @@
-import React, { useRef } from 'react';
-// import { useParams } from 'react-router-dom';
-import { 
-  // useSelector, 
-  useDispatch } from 'react-redux';
-import { addAnswer } from '../redux/Reducers/AC/addAnswerAC.js'
+// import React, { useRef } from 'react';
+// // import { useParams } from 'react-router-dom';
+// import { 
+//   // useSelector, 
+//   useDispatch } from 'react-redux';
+// import { addAnswer } from '../redux/Reducers/AC/addAnswerAC.js'
 
-function Question(props) {
-  // const { id } = useParams();
-  // const currentQuestion = useSelector(state => state.questions).filter(el => el.id === +id);
-  // const dispatch = useDispatch()
-  // const currentQuestion = { 
-  //   question: 'QUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTION', 
-  //   answer: 'ANSWERANSWERANSWER',
-  //   point: 300,
-  //   status: 'new'
-  // // }
-  // const answerRef = useRef();
-  // const inputHandler = () => {
-  //   const answer = answerRef.current.value;
-  //   dispatch(addAnswer(answer))
-  // }
+// function Question(props) {
+//   // const { id } = useParams();
+//   // const currentQuestion = useSelector(state => state.questions).filter(el => el.id === +id);
+//   // const dispatch = useDispatch()
+//   // const currentQuestion = { 
+//   //   question: 'QUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTIONQUESTION', 
+//   //   answer: 'ANSWERANSWERANSWER',
+//   //   point: 300,
+//   //   status: 'new'
+//   // // }
+//   // const answerRef = useRef();
+//   // const inputHandler = () => {
+//   //   const answer = answerRef.current.value;
+//   //   dispatch(addAnswer(answer))
+//   // }
 
 
-  return (
-    <div class="container">
-      <div class="row"> dfkjedfjejfe
-       <div class="input-field col s10">
-          <div>{currentQuestion.question}</div>
-             <input id="answer" type="text" ref="answerRef" class="validate"/>
-         <label class="active" for="answer">Ответ</label> 
-      <button class="waves-effect waves-light btn" onClick={inputHandler}>"Это окончательный ответ"</button> 
-        </div>
-       </div>
-     </div>
-  );
-}
+//   return (
+//     <div class="container">
+//       <div class="row"> dfkjedfjejfe
+//        <div class="input-field col s10">
+//           <div>{currentQuestion.question}</div>
+//              <input id="answer" type="text" ref="answerRef" class="validate"/>
+//          <label class="active" for="answer">Ответ</label> 
+//       <button class="waves-effect waves-light btn" onClick={inputHandler}>"Это окончательный ответ"</button> 
+//         </div>
+//        </div>
+//      </div>
+//   );
+// }
 
-export default Question;
+// export default Question;
