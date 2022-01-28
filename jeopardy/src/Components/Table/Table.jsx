19b0f6b605ccf241296f6565ef56e055c17d5b5a
Olga <anugallaguna@gmail.com>
2022-01-28 13:57:17 +0300

    bad commit

--- jeopardy/src/Components/Table/Table.jsx
+++ jeopardy/src/Components/Table/Table.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Row from '../Row/Row';
-import Question from '../Question'
 
 
 function Table(props) {
   return (
     <div>
       <Row />
-      <Question></Question>
     </div>
   );
 }
